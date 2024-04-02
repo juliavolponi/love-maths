9ed9cdddcb16582f3ef933b97bab666e021d88f2
julia volponi <juliavolponi@outlook.com>
2024-04-02 07:28:11 +0000

    feat: Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function CalculateCorrectAnswer() {
+
+}
+
+function IncrementScore() {
+
+}
+
+function IncrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
