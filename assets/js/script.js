1bffb86bf6ee5eb27aec1ac2c34bee09997fb3be
julia volponi <juliavolponi@outlook.com>
2024-04-02 09:16:54 +0000

    feat: Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,9 @@ function runGame(gameType) {
 
     if (gameType === 'addition') {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType=== 'multiply') {
+        displayMultiplyQuestion(num1,num2);
+    }else {
         alert(`Unknown game type: ${gameType}`);
         thrown `Unknown game type: ${gameType}. Aborting!`
     }
@@ -54,7 +56,10 @@ function CalculateCorrectAnswer() {
 
     if (operator === '+') {
         return [operand1 + operand2, 'addition'];
-    } else {
+    } else if(operator === 'x'){
+        return [operand1 * operand2, 'multiply'];
+
+    }else {
         alert(`unimplemented operator ${operator}`)
         thrown `Unimplemented operator ${operator}. Aborting!`
     }
@@ -88,6 +93,10 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent= operand1;
+    document.getElementById('operand2').textContent= operand2;
+    document.getElementById('operator').textContent= 'x';
+
 
 }
