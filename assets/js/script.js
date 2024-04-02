37e5321f4abb2b4666bd0061882e05a46997d747
julia volponi <juliavolponi@outlook.com>
2024-04-02 09:28:15 +0000

    feat: Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,9 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType=== 'multiply') {
         displayMultiplyQuestion(num1,num2);
+    }else if(gameType === 'subtraction') {
+        displaySubtractQuestion(num1,num2);
+
     }else {
         alert(`Unknown game type: ${gameType}`);
         thrown `Unknown game type: ${gameType}. Aborting!`
@@ -59,6 +62,9 @@ function CalculateCorrectAnswer() {
     } else if(operator === 'x'){
         return [operand1 * operand2, 'multiply'];
 
+    }else if (operator=== '-'){
+        return [operand1 - operand2, 'subtraction'];
+
     }else {
         alert(`unimplemented operator ${operator}`)
         thrown `Unimplemented operator ${operator}. Aborting!`
@@ -89,7 +95,12 @@ function displayAdditionQuestion(operand1,operand2) {
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) { 
+    document.getElementById('operand1').textContent= operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent= operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent= '-';
+
+
 
 }
 
